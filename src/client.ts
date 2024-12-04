aaeb336a24ecec53b45c5657cb34e472562f2903
yungbali <info.prodbybali@gmail.com>
2024-12-05 00:17:11 +0100

    v7

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,8 @@
 import { generateClient } from "aws-amplify/api";
-import { Schema } from "../amplify/data/resource";
 import { createAIHooks } from "@aws-amplify/ui-react-ai";
 
-export const client = generateClient<Schema>({ authMode: "userPool" });
-export const { useAIConversation, useAIGeneration } = createAIHooks(client); 
+export const client = generateClient();
+export const { useAIConversation, useAIGeneration } = createAIHooks(client);
+
+// Note: We'll need to install the AI package first
+// npm install @aws-amplify/ui-react-ai 
