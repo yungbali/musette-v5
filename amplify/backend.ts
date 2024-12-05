8daf09c972a3fd823d2a04d61d0c1204e69f0075
yungbali <info.prodbybali@gmail.com>
2024-12-05 14:42:26 +0100

    SOLID

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,10 +2,7 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import data from './data/resource';
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
+export const backend = defineBackend({
   auth,
-  data,
+  data
 });
