af6edba5570f61627783820cb5165ead3ad4cbf1
yungbali <info.prodbybali@gmail.com>
2024-12-05 14:33:55 +0100

    Amplify config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      './amplify_outputs.json': '/src/amplify_outputs.json',
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env
+    },
+    build: {
+      outDir: 'dist'
     }
-  },
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      external: ['aws-exports'],
-    }
-  }
+  };
 });
