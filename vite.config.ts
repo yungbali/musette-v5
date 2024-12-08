50a40fca47af9042f9384ef37355b4ed64c15e05
yungbali <info.prodbybali@gmail.com>
2024-12-08 16:56:58 +0100

    active

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': env
-    },
-    build: {
-      outDir: 'dist'
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@aws-amplify/data-schema-types']
+  },
+  resolve: {
+    alias: {
+      './builder': '@aws-amplify/data-schema-types/lib-esm/builder.js',
+      './client': '@aws-amplify/data-schema-types/lib-esm/client.js',
+      './util': '@aws-amplify/data-schema-types/lib-esm/util.js'
     }
-  };
+  },
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 });
